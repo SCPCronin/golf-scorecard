31703ea62808c4b78d5d7b73197bd7a8c5e7b150
Sean Cronin <cpcsean@gmail.com>
2023-09-16 22:22:13 +0100

    Render Setup Component if game is not running

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import Setup from './components/Setup';
 import './App.css';
 
 function App() {
+
+  const [isGameRunning, setIsGameRunning] = React.useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {!isGameRunning ? <Setup/> : <h1> Game Component not created yet. </h1>}
     </div>
   );
 }
---
+++ src/components/Setup.tsx
@@ -0,0 +1,14 @@
+import React, { ComponentProps } from 'react'
+
+
+function Setup () {
+    return (
+        <div>
+            <h1>
+                Setup Component Base
+            </h1>
+        </div>
+    )
+}
+
+export default Setup;

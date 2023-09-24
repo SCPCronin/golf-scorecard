199c6e16e19d2dec84fbecb64076ef811b17c901
Sean Cronin <cpcsean@gmail.com>
2023-09-24 19:17:58 +0100

    Optimize Setup.tsx

--- src/components/Setup.tsx
+++ src/components/Setup.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Box } from '@mui/material';
 import GameTypes from '../types/gameTypes';
-import SelectGameType from './Setup/Select_Gametype';
-import SelectCourse from './Setup/Select_Course'
 import Courses from '../types/Courses';
+import SelectCourse from './Setup/Select_Course'
+import SelectGameType from './Setup/Select_Gametype';
 import AllPlayers from './Setup/AllPlayers';
 import Buttons from './Setup/Buttons'
-import { Box, Button, Typography } from '@mui/material';
 import '../styles/setup.css';
 
 interface SetupProps {
@@ -18,7 +18,15 @@ interface SetupProps {
     setPlayerNames: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
-function Setup({setIsSetupMode, selectedGameType, setSelectedGameType, selectedCourse, setSelectedCourse, playerNames, setPlayerNames}: SetupProps) {
+function Setup({
+    setIsSetupMode,
+    selectedGameType,
+    setSelectedGameType,
+    selectedCourse,
+    setSelectedCourse,
+    playerNames,
+    setPlayerNames
+}: SetupProps) {
     
     const resetState = () => {
         setSelectedGameType(GameTypes.PITCH_AND_PUTT);
@@ -37,12 +45,12 @@ function Setup({setIsSetupMode, selectedGameType, setSelectedGameType, selectedC
                     selectedGameType={selectedGameType}
                     setSelectedGameType={setSelectedGameType}
                     />
-                {selectedGameType === GameTypes.GOLF && // For now, only show courses if the game type selected is Golf
+                {selectedGameType === GameTypes.GOLF && (// For now, only show courses if the game type selected is Golf
                     <SelectCourse
                         selectedCourse={selectedCourse}
                         setSelectedCourse={setSelectedCourse}
                     />
-                }
+                )}
                 <AllPlayers playerNames={playerNames} setPlayerNames={setPlayerNames}/>
                 <Buttons resetState={resetState} startGame={startGame} /> 
             </Box>

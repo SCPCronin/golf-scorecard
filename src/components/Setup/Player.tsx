20a7620602787eb42f79397515a9445073c42371
Sean Cronin <cpcsean@gmail.com>
2023-09-24 20:12:33 +0100

    Optimizing Player component

--- src/components/Setup/Player.tsx
+++ src/components/Setup/Player.tsx
@@ -18,7 +18,7 @@ function Player({playerName, setPlayerName}: PlayerProps) {
             onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
                 setPlayerName(event.target.value);
             }}
-            style={{marginTop: "25px"}}
+            style={{marginTop: "20px"}}
         />
     )
 }
